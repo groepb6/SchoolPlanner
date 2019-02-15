7aff19b0befc8eeebe1a9ee1e8bf5907c91b83ef
Dustin <dhendrik6@avans.nl>
2019-02-15 16:04:39 +0100

    Cleaned up the application settings

--- src/gui/settings/ApplicationSettings.java
+++ src/gui/settings/ApplicationSettings.java
@@ -5,6 +5,13 @@ import javafx.scene.text.Font;
 
 import java.util.ArrayList;
 
+/**
+ * @author Dustin Hendriks
+ * @since 04-02-2019
+ * This class enables easy reading of some basic application settings. If there are any specific settings such as text size or window sizes those settings can be added here.
+ * There currently is some obsolete data here, but this can be cleaned up in the final version.
+ */
+
 public class ApplicationSettings {
     public static final Color themeColor = Color.CORNFLOWERBLUE;
     public static final int minWidth = 200;
@@ -15,12 +22,4 @@ public class ApplicationSettings {
     public static final int standardTextSize = 10;
     public static final int headerTextSize = 20;
     public static final Font headerFont = new Font(javafx.scene.text.Font.getFamilies().get(7), 30);
-
-    public static ArrayList getTestNames() {
-        ArrayList<String> testNames = new ArrayList<String>();
-        for (int amountOfTestSubjects = 0; amountOfTestSubjects < 20; amountOfTestSubjects++) {
-            testNames.add("testSubject" + Integer.toString(amountOfTestSubjects));
-        }
-        return testNames;
-    }
 }
