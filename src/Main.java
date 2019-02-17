80dc8f521692bcaf5d947a832109b6db29a2eed0
Dustin <dhendrik6@avans.nl>
2019-02-17 13:19:37 +0100

    Added Plans in the GUI are always shown, even after closing and reopening the application.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import data.readwrite.DataReader;
 import data.readwrite.DataWriter;
 import data.schedulerelated.TempSchedule;
 import gui.SchoolManager;
@@ -14,24 +15,31 @@ import static javafx.application.Application.launch;
 
 public class Main {
     public static void main (String args[]) {
-        TempSchedule tempSchedule = new TempSchedule();
-        tempSchedule.saveAllSchedules(
-                new Plan("08:45 - 09:45", "23TIVT1B6", "LA424", "Johan", "OGP"),
-                new Plan("10:00 - 12:00", "23TIVT1B6", "LA218", "Pieter", "Maths"),
-                new Plan("13:30 - 14:00", "23TIVT1B6", "LA672", "Genevieve", "P&OC"),
-                new Plan("15:00 - 16:30", "23TIVT1B6", "LD726", "Jan", "OOM"),
-                new Plan("17:00 - 20:00", "23TIVT1B6", "HA300", "Peter", "HI"),
-                new Plan("09:00 - 10:00", "23TIVT1A5", "HD520", "Etiënne", "OGP"),
-                new Plan("12:00 - 14:00", "23TIVT1A2", "LD120", "Maurice", "OGP"),
-                new Plan("16:00 - 20:00", "23TIVT1C2", "HA130", "Jessica", "OOM"),
-                new Plan("12:00 - 13:00", "23TIVT1B6", "LD130", "Paul", "XP"),
-                new Plan("12:00 - 13:00", "23TIVT1C2", "LD130", "Paul", "OGP"),
-                new Plan("15:20 - 17:39", "23TIVT1A0", "LD212", "Hans", "Alg")
-        );
+        boolean noData = true;
         try {
-            DataWriter.writeObject(tempSchedule.getData());
-        } catch (Exception e) {e.printStackTrace();}
-
+            noData = (DataReader.readObject().size()<1);
+        } catch (Exception e) {}
+        if (noData) {
+            TempSchedule tempSchedule = new TempSchedule();
+            tempSchedule.saveAllSchedules(
+                    new Plan("08:45 - 09:45", "23TIVT1B6", "LA424", "Johan", "OGP"),
+                    new Plan("10:00 - 12:00", "23TIVT1B6", "LA218", "Pieter", "Maths"),
+                    new Plan("13:30 - 14:00", "23TIVT1B6", "LA672", "Genevieve", "P&OC"),
+                    new Plan("15:00 - 16:30", "23TIVT1B6", "LD726", "Jan", "OOM"),
+                    new Plan("17:00 - 20:00", "23TIVT1B6", "HA300", "Peter", "HI"),
+                    new Plan("09:00 - 10:00", "23TIVT1A5", "HD520", "Etiënne", "OGP"),
+                    new Plan("12:00 - 14:00", "23TIVT1A2", "LD120", "Maurice", "OGP"),
+                    new Plan("16:00 - 20:00", "23TIVT1C2", "HA130", "Jessica", "OOM"),
+                    new Plan("12:00 - 13:00", "23TIVT1B6", "LD130", "Paul", "XP"),
+                    new Plan("12:00 - 13:00", "23TIVT1C2", "LD130", "Paul", "OGP"),
+                    new Plan("15:20 - 17:39", "23TIVT1A0", "LD212", "Hans", "Alg")
+            );
+            try {
+                DataWriter.writeObject(tempSchedule.getData());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         launch(SchoolManager.class);
     }
 }
