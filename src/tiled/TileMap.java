8fa9a4b91d9f227665a695e3981078721065b5b1
Noah Walsmits <noahwalsmits@telfort.nl>
2019-02-20 15:46:40 +0100

    Getting on the right path I think

--- src/tiled/TileMap.java
+++ src/tiled/TileMap.java
@@ -1,25 +1,37 @@
 package tiled;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.List;
-import javax.json.*;
 
 public class TileMap {
+    private int imageHeight;
+    private int imageWidth;
     private List<Tile> tiles;
 
     public TileMap(String fileName) {
+        this.readTileMap(fileName);
+    }
 
+    public String toString() {
+        return imageHeight + "";
+    }
+
+    private void readTileMap(String fileName) {
         try {
-            File file = new File(fileName);
+            String filePath = TileMap.class.getResource("/tiled/" + fileName + ".json").toString();
+            File file = new File(filePath);
             InputStream input = new FileInputStream(file);
             JsonReader jsonReader = Json.createReader(input);
             JsonObject tileMap = jsonReader.readObject();
+            this.imageHeight = tileMap.getInt("imagewidth", 10);
         } catch (Exception e) {
-
+            System.out.println("dommet");
         }
-
     }
 
 
---
+++ src/tiled/TiledMain.java
@@ -0,0 +1,10 @@
+package tiled;
+
+public class TiledMain {
+
+    public static void main(String[] args) {
+        TileMap tm = new TileMap("Testj");
+        System.out.println(tm);
+    }
+
+}
