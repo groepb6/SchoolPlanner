ace01bfb9f66ccde90f7294151f425278e7df747
Noah Walsmits <noahwalsmits@telfort.nl>
2019-02-22 12:32:58 +0100

    Added TiledReader with TileMap reading

---
+++ src/tiled/TiledReader.java
@@ -0,0 +1,42 @@
+package tiled;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class TiledReader {
+
+    /**
+     * Loads a TileMap from a json file
+     *
+     * @param fileName The file name of the json file
+     * @return a JsonObject that can be used in the constructor of TileMap
+     */
+    public static JsonObject readTileMap(String fileName) {
+        String loadPath = "saves/groups/" + fileName + ".txt";
+        System.out.println("Attempting to load group from " + loadPath);
+        try {
+            File saveFile = new File(loadPath);
+            FileInputStream fileInputStream = new FileInputStream(saveFile);
+            JsonReader jsonReader = Json.createReader(fileInputStream);
+            return (JsonObject) jsonReader.readObject();
+        } catch (FileNotFoundException exception) {
+            System.out.println("Save file not found!");
+        }
+        System.out.println("Loading has failed!");
+        return null;
+    }
+
+    /**
+     * Loads a group from a file that has the standard file name, should only be used by developers
+     *
+     * @return Either a List of schedules or null
+     */
+    public static JsonObject readTileMap() {
+        return TiledReader.readTileMap("test.json");
+    }
+
+}
