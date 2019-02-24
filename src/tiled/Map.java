34ea77362c53fbd9cd34e4988fa0603fcb3fd78a
Noah Walsmits <noahwalsmits@telfort.nl>
2019-02-24 15:54:50 +0100

    Map constructor is now partially finished

--- src/tiled/Map.java
+++ src/tiled/Map.java
@@ -1,35 +1,68 @@
 package tiled;
 
+import javax.json.JsonArray;
 import javax.json.JsonObject;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Only compatible with Orthogonal maps
  */
 public class Map {
-    private String backgroundColor;
     private int height;
     private int width;
     private boolean infinite;
     private int tileHeight;
     private int tileWidth;
     private String renderOrder;
+    private String backgroundColor;
     private int nextLayerId;
     private int nextObjectId;
     private String orientation;
-    private String[] properties; //do we need this?
-    private TileMap[] tileSets;
+    private List<String> properties; //do we need this?
+    private List<TileSet> tileSets;
+    private List<Layer> layers;
     private String type;
 
-
-    private List<Layer> layers;
-
     public Map(JsonObject jsonMap) {
-
+        if (jsonMap.getString("type").equals("map")) {
+            this.height = jsonMap.getInt("height");
+            this.width = jsonMap.getInt("width");
+            this.infinite = jsonMap.getBoolean("infinite", false);
+            this.tileHeight = jsonMap.getInt("tileheight");
+            this.tileWidth = jsonMap.getInt("tilewidth");
+            this.renderOrder = jsonMap.getString("renderorder");
+            this.backgroundColor = jsonMap.getString("backgroundcolor");
+            this.nextLayerId = jsonMap.getInt("nextlayerid");
+            this.nextObjectId = jsonMap.getInt("nextobjectid");
+            this.orientation = jsonMap.getString("orientation");
+            this.addProperties(jsonMap);
+            this.addTileSets();
+            this.addLayers();
+            this.type = jsonMap.getString("type");
+        } else {
+            System.out.println("You tried loading a non-map file into a map object!");
+        }
     }
 
     public void draw() {
 
+    }
+
+    private void addProperties(JsonObject jsonMap) {
+        this.properties = new ArrayList<>();
+        JsonArray jsonArray = jsonMap.getJsonArray("properties");
+        for (int i = 0; i < jsonArray.size(); i++) {
+            this.properties.add(jsonArray.getString(i));
+        }
+    }
+
+    private void addTileSets() {
+        //todo have this method add TileSet objects
+    }
+
+    private void addLayers() {
+        //todo have this method add Layer objects
     }
 
 }
