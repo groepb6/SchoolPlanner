c8a034e9417e43d1a593c095bda18954a4e09b53
Noah Walsmits <noahwalsmits@telfort.nl>
2019-03-23 23:06:00 +0100

    Made emergencySchool method for DataReader
    The program crashes when it tries to read an school that is null. This would happen when it tried to read a saved school after the class was changed.

--- src/data/readwrite/DataReader.java
+++ src/data/readwrite/DataReader.java
@@ -1,8 +1,12 @@
 package data.readwrite;
 
 import data.persons.Teacher;
+import data.rooms.Classroom;
+import data.schedulerelated.Hour;
 import data.schedulerelated.Schedule;
+import data.schoolrelated.Group;
 import data.schoolrelated.School;
+import data.schoolrelated.Subject;
 
 import java.io.*;
 import java.util.List;
@@ -37,7 +41,8 @@ public class DataReader {
         }
 //        System.out.println("Loading has failed!");
 //        System.out.println("The program will probably stop working now.");
-        return null;
+//        return null;
+        return DataReader.emergencySchool();
     }
 
     public static School readPreset() {
@@ -60,6 +65,18 @@ public class DataReader {
         }
         System.out.println("Loading has failed!");
         System.out.println("The program will probably stop working now.");
-        return null;
+        return DataReader.emergencySchool();
     }
+
+    /**
+     * Creates a School object with useless info. Used to create a School object when the saved object cannot be properly read.
+     * @return A School object with no usable info.
+     */
+    public static School emergencySchool() { //TODO: This will most likely mess up any simulation!
+        School school = new School("");
+        Schedule schedule = new Schedule(Hour.NONE, new Group(""), new Classroom(""), new Teacher(""), new Subject(""));
+        school.addSchedule(schedule);
+        return school;
+    }
+
 }
