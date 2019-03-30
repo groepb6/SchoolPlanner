3d02bd9853fdad4bc8e702fbf0129f5b1622a8bf
Noah Walsmits <noahwalsmits@telfort.nl>
2019-03-30 19:01:16 +0100

    Base integration Sim and SimUpdate

--- src/simulation/sims/Sim.java
+++ src/simulation/sims/Sim.java
@@ -1,10 +1,12 @@
 package simulation.sims;
 
+import data.objects.Chair;
 import gui.settings.ApplicationSettings;
 import javafx.scene.canvas.Canvas;
 import org.jfree.fx.FXGraphics2D;
 import simulation.data.Area;
 import simulation.pathfinding.Node;
+
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
@@ -16,7 +18,7 @@ public class Sim {
     int index = 0;
     private double angle;
     private double targetAngle = 0;
-    private double speed;
+    private int speed;
     Point2D currentPos;
     Point2D targetPos;
     private FXGraphics2D g2d;
@@ -27,19 +29,25 @@ public class Sim {
     public ArrayList<Area> areas;
     private int targetArea;
     private int oldTargetArea;
+    private int originalSpeed;
     public String name;
+    private Chair chair;
+    public boolean gotoChair = false;
+    private boolean horizontalFirst = true;
+    private Chair.Direction chairDir;
 
     public Sim(Point2D startPos, FXGraphics2D g2d, SimSkin simSkin, Canvas canvas, ArrayList<Area> areas, String name) {
         this.currentPos = startPos;
         this.targetPos = startPos;
-        this.speed = ApplicationSettings.SIMDEFAULTSPEED;
+        this.speed = (int) (Math.random() * 2) + 4;
+        this.originalSpeed = speed;
         this.g2d = g2d;
         this.simSkin = simSkin;
         this.canvas = canvas;
         this.angle = Math.random() * Math.PI * 2;
         this.targetPos = new Point2D.Double(500, 500);
         this.areas = areas;
-        this.targetArea = (int)(Math.random()*areas.size()-1);
+        this.targetArea = (int) (Math.random() * areas.size() - 1);
         this.name = name;
     }
 
@@ -52,35 +60,39 @@ public class Sim {
     }
 
     public void update(Sim[] sims, Node[][] collisionNodes) {
-        Point2D newPos = new Point2D.Double(currentPos.getX() + this.speed * Math.cos(angle), currentPos.getY() + this.speed * Math.sin(angle));
-        this.sims = sims;
-        boolean hasCollision = false;
-        for (Sim sim : sims) {
-            if (sim != this && sim.simCollision(newPos)) {
-                hasCollision = true;
-                break;
+        if (!gotoChair) {
+            Point2D newPos = new Point2D.Double(currentPos.getX() + this.speed * Math.cos(angle), currentPos.getY() + this.speed * Math.sin(angle));
+            this.sims = sims;
+            boolean hasCollision = false;
+            for (Sim sim : sims) {
+                if (sim != this && sim.simCollision(newPos)) {
+                    hasCollision = true;
+                    break;
+                }
             }
-        }
 
-        boolean onTarget = currentPos.distance(targetPos) < speed;
-        if (!onTarget) {
-            if (!isOutOfBounds(newPos))
-                if (!hasCollision && collisionNodes[(int)Math.round(newPos.getX() / 32)][(int)Math.round(newPos.getY() / 32)].walkable)
-                    currentPos = newPos;
-                else angle += 1.5 * aggressionFactorInBehaviour;
-            Point2D difference = new Point2D.Double(targetPos.getX() - currentPos.getX(), targetPos.getY() - currentPos.getY());
-            double targetAngle = Math.atan2(difference.getY(), difference.getX());
-            double angleDifference = targetAngle - angle;
-            while (angleDifference > Math.PI) angleDifference -= 2 * Math.PI;
-            while (angleDifference < -Math.PI) angleDifference += 2 * Math.PI;
-            if (angleDifference < -aggressionFactorInBehaviour)
-                angle -= aggressionFactorInBehaviour;
-            else if (angleDifference > aggressionFactorInBehaviour)
-                angle += aggressionFactorInBehaviour;
-            else angle = targetAngle;
-            setFrame();
-        }
+            boolean onTarget = currentPos.distance(targetPos) < speed;
+            if (!onTarget) {
+                if (!isOutOfBounds(newPos))
+                    if (!hasCollision && collisionNodes[(int) Math.round(newPos.getX() / 32)][(int) Math.round(newPos.getY() / 32)].walkable)
+                        currentPos = newPos;
+                    else angle += 1.5 * aggressionFactorInBehaviour;
+                Point2D difference = new Point2D.Double(targetPos.getX() - currentPos.getX(), targetPos.getY() - currentPos.getY());
+                double targetAngle = Math.atan2(difference.getY(), difference.getX());
+                double angleDifference = targetAngle - angle;
+                while (angleDifference > Math.PI) angleDifference -= 2 * Math.PI;
+                while (angleDifference < -Math.PI) angleDifference += 2 * Math.PI;
+                if (angleDifference < -aggressionFactorInBehaviour)
+                    angle -= aggressionFactorInBehaviour;
+                else if (angleDifference > aggressionFactorInBehaviour)
+                    angle += aggressionFactorInBehaviour;
+                else angle = targetAngle;
+                setFrame();
+            }
+        } else
+            searchChair();
     }
+//    }
 
     private void setFrame() {
         double angleRad = angle / Math.PI;
@@ -89,7 +101,6 @@ public class Sim {
         if (angleRad > 0.75) {
             angleRad = angleRad - 2;
         }
-
         int rotatedAngle = (int) ((((angleRad) * 2) + 2.5)); // 0 t/m 7
         switch (rotatedAngle) {
             case 0:
@@ -110,22 +121,85 @@ public class Sim {
         }
     }
 
+    private void searchChair() {
+        if (horizontalFirst) {
+            if (!(Math.abs(currentPos.getX() - targetPos.getX()) <= speed)) {
+                if (currentPos.getX() < targetPos.getX()) {
+                    currentPos.setLocation(currentPos.getX() + speed, currentPos.getY());
+                    setSimSkinDir(simSkin.walkRight(this));
+                } else if (currentPos.getX() > targetPos.getX()) {
+                    currentPos.setLocation(currentPos.getX() - speed, currentPos.getY());
+                    setSimSkinDir(simSkin.walkLeft(this));
+                }
+            } else {
+                if (Math.abs(currentPos.getY() - targetPos.getY()) <= speed) {
+                    setSimFrame();
+                } else if (currentPos.getY() < targetPos.getY()) {
+                    currentPos.setLocation(currentPos.getX(), currentPos.getY() + speed);
+                    setSimSkinDir(simSkin.walkDown(this));
+                } else if (currentPos.getY() > targetPos.getY()) {
+                    currentPos.setLocation(currentPos.getX(), currentPos.getY() - speed);
+                    setSimSkinDir(simSkin.walkUp(this));
+                }
+            }
+        } else {
+            if (!(Math.abs(currentPos.getY() - targetPos.getY()) <= speed)) {
+                if (currentPos.getY() < targetPos.getY()) {
+                    currentPos.setLocation(currentPos.getX(), currentPos.getY() + speed);
+                    setSimSkinDir(simSkin.walkDown(this));
+                } else if (currentPos.getY() > targetPos.getY()) {
+                    currentPos.setLocation(currentPos.getX(), currentPos.getY() - speed);
+                    setSimSkinDir(simSkin.walkUp(this));
+                }
+            } else {
+                if (Math.abs(currentPos.getX() - targetPos.getX()) <= speed) {
+                    setSimFrame();
+                } else if (currentPos.getX() < targetPos.getX()) {
+                    currentPos.setLocation(currentPos.getX() + speed, currentPos.getY());
+                    setSimSkinDir(simSkin.walkRight(this));
+                } else if (currentPos.getX() > targetPos.getX()) {
+                    currentPos.setLocation(currentPos.getX() - speed, currentPos.getY());
+                    setSimSkinDir(simSkin.walkLeft(this));
+                }
+            }
+        }
+    }
+
+    private void setSimFrame() {
+        switch (this.chairDir) {
+            case RIGHT:
+                setSimSkinDir(simSkin.stationaryRight());
+                break;
+            case LEFT:
+                setSimSkinDir(simSkin.stationaryLeft());
+                break;
+            case UP:
+                setSimSkinDir(simSkin.stationaryUp());
+                break;
+            case DOWN:
+                setSimSkinDir(simSkin.stationaryDown());
+                break;
+            default:
+                setSimSkinDir(simSkin.stationaryUp());
+        }
+    }
+
     public Point2D getCurrentPos() {
         return this.currentPos;
     }
 
-    public double getSpeed() {
+    public int getSpeed() {
         return this.speed;
     }
 
     public void draw() {
         AffineTransform affineTransform = new AffineTransform();
-        affineTransform.translate(currentPos.getX()-16, currentPos.getY() - 32 - 28); // extra -28 to translate to feet
+        affineTransform.translate(currentPos.getX() - 16, currentPos.getY() - 16 - 28); // extra -28 to translate to feet
         g2d.drawImage(bufferedImage, affineTransform, null);
     }
 
     private boolean isOutOfBounds(Point2D pos) {
-        return (pos.getX() < 0 || pos.getX() >= canvas.getWidth() || pos.getY() < 0 || pos.getY() >= canvas.getHeight());
+        return (pos.getX() < 0 || pos.getX() >= canvas.getWidth() - 32 || pos.getY() < 0 || pos.getY() >= canvas.getHeight() - 32);
     }
 
     private boolean isBetterNode(int score, int currentPosX, int currentPosY, Node[][] nodes, int maxWidth, int maxHeight) {
@@ -135,62 +209,81 @@ public class Sim {
     }
 
     public boolean isInTargetArea() {
-        return (((currentPos.getX() > areas.get(targetArea).x) && currentPos.getX() < (areas.get(targetArea).x+areas.get(targetArea).areaWidth)) && ((currentPos.getY() > areas.get(targetArea).y)) && currentPos.getY() < (areas.get(targetArea).y+areas.get(targetArea).areaHeight));
+        return (((currentPos.getX() > areas.get(targetArea).x) && currentPos.getX() < (areas.get(targetArea).x + areas.get(targetArea).areaWidth)) && ((currentPos.getY() > areas.get(targetArea).y)) && currentPos.getY() < (areas.get(targetArea).y + areas.get(targetArea).areaHeight));
     }
 
     public void pathFind(Node[][] nodes) {
-        int maxWidth = (int) (canvas.getWidth() / 32);
-        int maxHeight = (int) (canvas.getHeight() / 32);
-        int currentPosX = (int) Math.round(currentPos.getX() / 32);
-        int currentPosY = (int) Math.round(currentPos.getY() / 32);
-        Node bestNode;
-        bestNode = nodes[currentPosX][currentPosY];
+        if (!gotoChair) {
+            int maxWidth = (int) (canvas.getWidth() / 32);
+            int maxHeight = (int) (canvas.getHeight() / 32);
+            int currentPosX = (int) Math.round(currentPos.getX() / 32);
+            int currentPosY = (int) Math.round(currentPos.getY() / 32);
+            Node bestNode;
+            bestNode = nodes[currentPosX][currentPosY];
 
-        if (bestNode.scores[targetArea] < 0)
-            bestNode.scores[targetArea] = Integer.MAX_VALUE;
+            if (bestNode.scores[targetArea] < 0)
+                bestNode.scores[targetArea] = Integer.MAX_VALUE;
 
-        if (isBetterNode(bestNode.scores[targetArea], currentPosX - 1, currentPosY, nodes, maxWidth, maxHeight)) {
-            if (bestNode.scores[targetArea] == Integer.MAX_VALUE)
-                bestNode.scores[targetArea] = -1;
-            bestNode = nodes[currentPosX - 1][currentPosY];
+            if (isBetterNode(bestNode.scores[targetArea], currentPosX - 1, currentPosY, nodes, maxWidth, maxHeight)) {
+                if (bestNode.scores[targetArea] == Integer.MAX_VALUE)
+                    bestNode.scores[targetArea] = -1;
+                bestNode = nodes[currentPosX - 1][currentPosY];
+            }
+            if (isBetterNode(bestNode.scores[targetArea], currentPosX + 1, currentPosY, nodes, maxWidth, maxHeight)) {
+                if (bestNode.scores[targetArea] == Integer.MAX_VALUE)
+                    bestNode.scores[targetArea] = -1;
+                bestNode = nodes[currentPosX + 1][currentPosY];
+            }
+            if (isBetterNode(bestNode.scores[targetArea], currentPosX, currentPosY - 1, nodes, maxWidth, maxHeight)) {
+                if (bestNode.scores[targetArea] == Integer.MAX_VALUE)
+                    bestNode.scores[targetArea] = -1;
+                bestNode = nodes[currentPosX][currentPosY - 1];
+            }
+            if (isBetterNode(bestNode.scores[targetArea], currentPosX, currentPosY + 1, nodes, maxWidth, maxHeight)) {
+                if (bestNode.scores[targetArea] == Integer.MAX_VALUE)
+                    bestNode.scores[targetArea] = -1;
+                bestNode = nodes[currentPosX][currentPosY + 1];
+            }
+            this.targetPos = new Point2D.Double((int) bestNode.getPosition().getX() * 32, (int) bestNode.getPosition().getY() * 32);
         }
-        if (isBetterNode(bestNode.scores[targetArea], currentPosX + 1, currentPosY, nodes, maxWidth, maxHeight)) {
-            if (bestNode.scores[targetArea] == Integer.MAX_VALUE)
-                bestNode.scores[targetArea] = -1;
-            bestNode = nodes[currentPosX + 1][currentPosY];
-        }
-        if (isBetterNode(bestNode.scores[targetArea], currentPosX, currentPosY - 1, nodes, maxWidth, maxHeight)) {
-            if (bestNode.scores[targetArea] == Integer.MAX_VALUE)
-                bestNode.scores[targetArea] = -1;
-            bestNode = nodes[currentPosX][currentPosY - 1];
-        }
-        if (isBetterNode(bestNode.scores[targetArea], currentPosX, currentPosY + 1, nodes, maxWidth, maxHeight)) {
-            if (bestNode.scores[targetArea] == Integer.MAX_VALUE)
-                bestNode.scores[targetArea] = -1;
-            bestNode = nodes[currentPosX][currentPosY + 1];
-        }
-        this.targetPos = new Point2D.Double((int) bestNode.getPosition().getX()*32, (int) bestNode.getPosition().getY() * 32);
+    }
+
+    public void gotoChair(Point2D targetPos, Chair.Direction direction, Chair chair) {
+        if (chair.direction == Chair.Direction.DOWN)
+            this.targetPos = new Point2D.Double(targetPos.getX() * 32, targetPos.getY() * 32 + 16);
+        else this.targetPos = new Point2D.Double(targetPos.getX() * 32, targetPos.getY() * 32);
+        this.chair = chair;
+        this.gotoChair = true;
+        horizontalFirst = ApplicationSettings.getHorizontalFirst(areas.get(targetArea).areaName);
+        chairDir = direction;
     }
 
     public int getTargetArea() {
         return targetArea;
     }
 
+    public void setTargetArea(Area area) {
+        this.setTargetArea(area.areaID);
+    }
+
     public void setTargetArea(int areaNumber) {
+        destroyAllTargets();
         this.oldTargetArea = targetArea;
         this.targetArea = areaNumber;
     }
 
-    public void setTargetArea(Area area) {
-        this.targetArea = area.areaID;
-    }
-
-    public void setSimSpeed(double speed) { //TODO: check
-        this.speed = speed;
+    private void destroyAllTargets() {
+        gotoChair = false;
+        if (chair != null)
+            chair.isAvailable = true;
     }
 
     public void setOldTargetArea() {
-        this.targetArea=oldTargetArea;
+        this.targetArea = oldTargetArea;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
     }
 
     private boolean simCollision(Point2D otherPos) {
