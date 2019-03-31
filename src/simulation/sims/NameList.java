f0a33cb1f8b5b84fab4d5324539ca8b60908da4f
Dustin <dhendrik6@avans.nl>
2019-03-31 22:18:17 +0200

    Added commentary

---
+++ src/simulation/sims/NameList.java
@@ -0,0 +1,38 @@
+package simulation.sims;
+import gui.settings.ApplicationSettings;
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ * @author Dustin Hendriks
+ * The NameList class grabs a name out of a generated list for Sims.
+ */
+
+public class NameList {
+    private Scanner scanner;
+
+    /**
+     * Create the scanner in the constructor so getName() can respond asap.
+     */
+
+    public NameList() {
+        try {
+            scanner = new Scanner(new File(getClass().getResource(ApplicationSettings.namePath).getPath()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Receive the first next name out of the list.
+     * @return Receive the next name out of the list.
+     */
+
+    public String getName() {
+        if (scanner != null)
+            if (scanner.hasNext())
+                return scanner.nextLine().trim();
+        return null;
+    }
+
+}
