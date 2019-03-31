ff7dc1af36ff7dc5aa40c9e28e81feeba6ab67de
Dustin <dhendrik6@avans.nl>
2019-03-31 22:13:07 +0200

    Commentary was added

--- src/data/readwrite/DataReader.java
+++ src/data/readwrite/DataReader.java
@@ -1,25 +1,74 @@
 package data.readwrite;
 
-import data.persons.Teacher;
-import data.schedulerelated.Schedule;
 import data.schoolrelated.School;
+import gui.settings.ApplicationSettings;
+import javafx.scene.Node;
 
 import java.io.*;
-import java.util.List;
 
 /**
  * Class to load objects saved to a file
+ *
+ * @author Hanno Brandwijk
+ * @author Wout Stevens
+ * @author Noah Walsmits
  */
 public class DataReader {
 
+    /**
+     * This method reads school.txt and returns a School-object when found.
+     * It also prints custom messages to respresent the different exceptions that could occur.
+     */
+
     public static School readSchool() {
-        String loadPath = "saves/school/school.txt";
         try {
-            File saveFile = new File(loadPath);
+            File saveFile = new File(ApplicationSettings.schoolPath);
             FileInputStream fileInputStream = new FileInputStream(saveFile);
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
             return (School) objectInputStream.readObject();
-        } catch (Exception exception) { exception.printStackTrace(); }
+        } catch (Exception e) { e.printStackTrace(); }
+        return null;
+    }
+
+    public static School readDefaults() {
+        try {
+            File saveFile = new File(ApplicationSettings.saveSlotPath);
+            FileInputStream fileInputStream = new FileInputStream(saveFile);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            return (School) objectInputStream.readObject();
+        } catch (FileNotFoundException exception) {
+            System.out.println("Save file not found!");
+            System.out.println("YOUR FILE COULD NOT BE LOADED!");
+        } catch (IOException exception) {
+            System.out.println("An IOException has occurred!");
+            System.out.println("YOUR FILE COULD NOT BE LOADED!");
+        } catch (ClassNotFoundException exception) {
+            System.out.println("No object found in file!");
+            System.out.println("YOUR FILE COULD NOT BE LOADED!");
+        }
+        System.out.println("Loading has failed!");
+        System.out.println("The program will probably stop working now.");
+        return null;
+    }
+
+    public static Node[][] readAllNodes() {
+        try {
+            File saveFile = new File(ApplicationSettings.saveNodePath);
+            FileInputStream fileInputStream = new FileInputStream(saveFile);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            return (Node[][]) objectInputStream.readObject();
+        } catch (FileNotFoundException exception) {
+            System.out.println("Save file not found!");
+            System.out.println("YOUR FILE COULD NOT BE LOADED!");
+        } catch (IOException exception) {
+            System.out.println("An IOException has occurred!");
+            System.out.println("YOUR FILE COULD NOT BE LOADED!");
+        } catch (ClassNotFoundException exception) {
+            System.out.println("No object found in file!");
+            System.out.println("YOUR FILE COULD NOT BE LOADED!");
+        }
+        System.out.println("Loading has failed!");
+        System.out.println("The program will probably stop working now.");
         return null;
     }
 }
--- src/data/readwrite/DataWriter.java
+++ src/data/readwrite/DataWriter.java
@@ -1,21 +1,69 @@
 package data.readwrite;
+
 import data.schoolrelated.School;
+import gui.settings.ApplicationSettings;
+import javafx.scene.Node;
+
 import java.io.*;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
 
 /**
  * Class to save objects to a file
+ * <p>
+ * @author Hanno Brandwijk
+ * @author Wout Stevens
+ * @author Noah Walsmits
  */
 public class DataWriter {
 
+    /**
+     * This method writes the given School-object to the file school.txt
+     * It also prints custom messages to respresent the different exceptions that could occur.
+     *
+     * @param school School object that contains all plans and schedules.
+     */
+
     public static void writeSchool(School school) {
-        String savePath = "saves/school/school.txt";
         try {
-            File saveFile = new File(savePath);
+            File saveFile = new File(ApplicationSettings.schoolPath);
             FileOutputStream fileOutputStream = new FileOutputStream(saveFile);
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
             objectOutputStream.writeObject(school);
-        } catch (Exception exception) { exception.printStackTrace(); }
+        } catch (Exception e) { e.printStackTrace(); }
+    }
+
+    public static void writeDefaults(School school) {
+        try {
+            File saveFile = new File(ApplicationSettings.saveSlotPath);
+            FileOutputStream fileOutputStream = new FileOutputStream(saveFile);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(school);
+            System.out.println("Object saved successfully.");
+        } catch (FileNotFoundException exception) {
+            System.out.println("Save destination not found!");
+            System.out.println("YOUR FILE MIGHT NOT HAVE BEEN SAVED!");
+            System.out.println("Please try again.");
+        } catch (IOException exception) {
+            System.out.println("An IOException has occurred!");
+            System.out.println("YOUR FILE MIGHT NOT HAVE BEEN SAVED!");
+            System.out.println("Please try again.");
+        }
+    }
+
+    public static void writeAllNodes(Node[][] allNodes) {
+        try {
+            File saveFile = new File(ApplicationSettings.saveNodePath);
+            FileOutputStream fileOutputStream = new FileOutputStream(saveFile);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(allNodes);
+            System.out.println("Object saved successfully.");
+        } catch (FileNotFoundException exception) {
+            System.out.println("Save destination not found!");
+            System.out.println("YOUR FILE MIGHT NOT HAVE BEEN SAVED!");
+            System.out.println("Please try again.");
+        } catch (IOException exception) {
+            System.out.println("An IOException has occurred!");
+            System.out.println("YOUR FILE MIGHT NOT HAVE BEEN SAVED!");
+            System.out.println("Please try again.");
+        }
     }
 }
